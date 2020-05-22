75dc4117de7c04d01b6ef6ba2b4909994c558601
Madhusudhanachari Alluri <madhuveer75.mv@gmail.com>
2020-05-22 18:36:51 +0000

    gitlab

---
+++ src/main/java/com/prodapt/services/EmployeeService.java
@@ -0,0 +1,33 @@
+package com.prodapt.services;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeService {
+
+	
+	@RequestMapping(value = "/getEmployeeDetails", method = RequestMethod.GET)
+	@ResponseBody
+	String uploadImage(HttpServletRequest request, HttpServletResponse response, HttpSession httpSession)
+			throws JSONException {
+
+		JSONObject js = new JSONObject();
+		js.put("Name", "Madhusudhanachari");
+		js.put("Calling Name", "Madhu");
+		js.put("DOB", "09-11-1993");
+		js.put("Position", "Sr.Software Engineer");
+		js.put("IT Services", "Cloud Platform");
+
+		return js.toString();
+}
+}
